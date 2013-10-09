dd942568208a546b96799a28b01197ed6aa27f3a
asiram <msairniksua@gmail.com>
2013-10-09 23:01:21 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidekiqTest::Application.config.secret_token = 'f4d52b11737a53d87a6c6ecdf9f3290b3c1e8448336fe3670b8e19f3bfc5ca8bae84ba751caf1d77651a86d226caad26a8523506896141485f6c710734ea4044'
